feabebd1df7a6136710b8721dcfda0fd0fef990d
Corbin Crutchley <git@crutchcorn.dev>
2022-12-15 07:10:01 -0800

    chore: add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
     return (
         <Html>
-            <Head lang="en" />
+            <Head lang="en">
+                <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+            </Head>
             <body>
                 <div className="screen">
                     <div className='screen-contents'>
